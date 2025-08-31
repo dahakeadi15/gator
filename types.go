f4bf394189eac9d8808f14ef1c2ce4d676577516
Aditya Dahake <dahakeadi15@gmail.com>
2025-08-31 23:50:32 +0530

    feat: add commands struct and methods - run and register

--- types.go
+++ types.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/dahakeadi15/gator/internal/config"
+import (
+	"fmt"
+
+	"github.com/dahakeadi15/gator/internal/config"
+)
 
 type state struct {
 	cfg *config.Config
@@ -9,4 +13,26 @@ type state struct {
 type command struct {
 	name      string
 	arguments []string
+}
+
+type commands struct {
+	functions map[string]func(s *state, cmd command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.functions[cmd.name]
+	if !exists {
+		return fmt.Errorf("command '%s' does not exist", cmd.name)
+	}
+
+	err := handler(s, cmd)
+	if err != nil {
+		return fmt.Errorf("error while running command '%s': %w", cmd.name, err)
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.functions[name] = f
 }
