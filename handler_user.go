a89aa0e2a9a1bfe61517884c1aff216148c1e0b5
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-04 10:34:24 +0530

    chore: improve reset cmd logging

--- handler_user.go
+++ handler_user.go
@@ -65,15 +65,11 @@ func handlerRegister(s *state, cmd command) error {
 }
 
 func handlerReset(s *state, cmd command) error {
-	if len(cmd.Arguments) != 0 {
-		return fmt.Errorf("usage: %s", cmd.Name)
-	}
-
 	err := s.db.DeleteAllUsers(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't reset database: %w", err)
+		return fmt.Errorf("couldn't delete users: %w", err)
 	}
-
+	fmt.Println("Database reset successfully!")
 	return nil
 }
 
