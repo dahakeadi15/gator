8435fc3c7909eb5d675614ba57d26fb6cd6c0117
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-05 23:52:22 +0530

    feat: add fetchFeed function

--- rss_feed.go
+++ rss_feed.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 type RSSFeed struct {
 	Channel struct {
 		Title       string    `xml:"title"`
@@ -14,4 +22,32 @@ type RSSItem struct {
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error sending http request: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error reading data: %w", err)
+	}
+
+	var feed RSSFeed
+	if err := xml.Unmarshal(data, &feed); err != nil {
+		return &RSSFeed{}, fmt.Errorf("error converting from json: %w", err)
+	}
+
+	return &feed, nil
 }
