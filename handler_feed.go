f3286c0434daad9267f76a6195b9203ad1a644b2
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-06 01:24:28 +0530

    refactor: proper printing of newly feed to console

--- handler_feed.go
+++ handler_feed.go
@@ -10,20 +10,19 @@ import (
 )
 
 func handlerAddFeed(s *state, cmd command) error {
+	currUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("error getting current user: %w", err)
+	}
+
 	if len(cmd.Arguments) != 2 {
 		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
 	}
 	name := cmd.Arguments[0]
 	url := cmd.Arguments[1]
 
-	// get current user from db
-	currUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("error getting current user: %w", err)
-	}
-
 	// add feed to db
-	newRSSFeed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+	newFeed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
@@ -32,10 +31,21 @@ func handlerAddFeed(s *state, cmd command) error {
 		UserID:    currUser.ID,
 	})
 	if err != nil {
-		return fmt.Errorf("error adding feed: %+v", err)
+		return fmt.Errorf("couldn't create feed: %w", err)
 	}
 
-	fmt.Printf("newFeed: \n%+v\n", newRSSFeed)
+	fmt.Println("Feed created successfully:")
+	printFeed(newFeed)
+	fmt.Println("==========================")
 
 	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf(" * ID:        %s\n", feed.ID)
+	fmt.Printf(" * Created:   %s\n", feed.CreatedAt)
+	fmt.Printf(" * Updated:   %s\n", feed.UpdatedAt)
+	fmt.Printf(" * Name:      %s\n", feed.Name)
+	fmt.Printf(" * URL:       %s\n", feed.Url)
+	fmt.Printf(" * UserId:    %s\n", feed.UserID)
 }
