f48742bb975592812f14c1906c2bac529048e267
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-06 18:44:51 +0530

    chore: tidy middleware

--- middlewares.go
+++ middlewares.go
@@ -8,15 +8,12 @@ import (
 )
 
 func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
-	return func(s *state, c command) error {
-		currUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	return func(s *state, cmd command) error {
+		user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
 		if err != nil {
 			return fmt.Errorf("couldn't find current user: %w", err)
 		}
-		if err := handler(s, c, currUser); err != nil {
-			return err
-		}
-		return nil
+		return handler(s, cmd, user)
 	}
 
 }
