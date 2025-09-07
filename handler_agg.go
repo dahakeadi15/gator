2bf6cc93bd400d383556cf760f248a4e641d5a8a
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-07 19:03:17 +0530

    feat: implement aggregator to fetch at a specified interval

--- handler_agg.go
+++ handler_agg.go
@@ -2,15 +2,60 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/dahakeadi15/gator/internal/database"
 )
 
 func handlerAggregator(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Arguments) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>\nwhere, time_between_reqs = 1s, 1m, 1h, etc", cmd.Name)
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.Arguments[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse duration: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenReqs.String())
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+	// return nil
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't fetch next feed: %w", err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		LastFetchedAt: sql.NullTime{
+			Time:  time.Now().UTC(),
+			Valid: true,
+		},
+		ID: nextFeed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't mark feed as fetched: %w", err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), nextFeed.Url)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("feed: \n%+v\n", *feed)
+	fmt.Printf("Fetched %d titles from feed %s:\n", len(rssFeed.Channel.Item), rssFeed.Channel.Title)
+	for _, rssItem := range rssFeed.Channel.Item {
+		fmt.Println(rssItem.Title)
+	}
+	fmt.Println("==============================")
+
 	return nil
 }
